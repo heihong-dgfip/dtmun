32c05c057db871a2be08dab5f2828bc909f83975
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-07-11 14:29:51 +0200

    add debug
    add debug

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,9 +1,9 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, Prop, h, ComponentInterface,Element } from '@stencil/core';
+
 import '@gouvfr/dsfr/dist/dsfr.module.min'
  
 declare global {
-  interface Window { dsfr: any; }
+  interface Window { dsfr: {start?:Function, verbose:any, inspector?:{tree}, accordion?}; }
 }
 
 
@@ -12,11 +12,13 @@ declare global {
   styleUrl: 'my-component.css',
   shadow: true,
 })
-export class MyComponent {
+export class MyComponent implements ComponentInterface {
   /**
    * The first name
    */
   @Prop() first: string;
+
+  @Element() el!: HTMLElement;
 
   /**
    * The middle name
@@ -28,25 +30,25 @@ export class MyComponent {
    */
   @Prop() last: string;
 
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
-  print():any{
+
+
+  componentDidLoad(){
     console.log(window.dsfr)
-    
+    window.dsfr.verbose =  true;
+    console.log(this.el.shadowRoot.querySelector('.fr-accordion'));
+    console.log(window.dsfr.accordion)
   }
 
+
   render() {
-    this.print();
+   
     return <section class="fr-accordion">
-      <h4>
-        <button class="fr-accordion__btn" aria-expanded="false" aria-controls='accordion1'>
-          Titre de accordion
-        </button>
-      </h4>
-      <div class="fr-collapse" id='accordion1'>
-          <p>description</p>
-      </div>
-    </section>;
+    <h3 class="fr-accordion__title">
+        <button class="fr-accordion__btn" aria-expanded="false" aria-controls="accordion-93">Intitulé accordéon</button>
+    </h3>
+    <div class="fr-collapse" id="accordion-93">
+       description
+    </div>
+  </section>;
   }
 }
